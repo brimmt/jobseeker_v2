bd388427aa14ed2e44d5e69a7f311de6b3dc45c2
Tatiana Brimm <tbrimm1@student.gsu.edu>
2026-05-13 20:06:56 -0400

    Created full login and signup system. Next step is frontend

--- jobseeker_backend/accounts/urls.py
+++ jobseeker_backend/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import MeView
+from .views import MeView, signup, signin
 
 urlpatterns = [
     path("me/", MeView.as_view(), name="me"),
+    path("signup/", signup, name="signup"),
+    path("login/", signin, name="login")
 ]
--- jobseeker_backend/accounts/views.py
+++ jobseeker_backend/accounts/views.py
@@ -66,5 +66,37 @@ def signup(request):
  
 
 
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def signin(request):
+    serializer = LoginInSerializer(data=request.data)    #I don't know why im adding this, i guess im validating that this is an actual serilizer
 
- 
+    if not serializer.is_valid():
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    data = serializer.validated_data
+
+    try:
+        auth_response = supabase.auth.sign_in_with_password({
+            "email": data["email"],
+            "password": data["password"]
+        })
+
+        if not auth_response.session:
+            return Response(
+                {"detail": "Invalid credentials."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        
+        return Response({
+            "message": "Sign in successful",
+            "access_token": auth_response.session.access_token,
+            "refresh_token": auth_response.session.refresh_token,
+        })
+    except Exception:
+        return Response({
+            "detail": "Login failed."
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+
+#I know auth header also goes somethere here since the user is being logged in i THINk this si where i'd have  supbase create the JWT token. 
